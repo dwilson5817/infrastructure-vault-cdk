7e1f5cd283a251f6d5c6373142aa6be8ff3ee9e0
Dylan Wilson <dwilson5817@gmail.com>
2023-12-25 21:15:36 +0000

    Initial commit

---
+++ test/infrastructure-vault.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as InfrastructureVault from '../lib/infrastructure-vault-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/infrastructure-vault-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new InfrastructureVault.InfrastructureVaultStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
