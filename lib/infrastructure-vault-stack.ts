e28cc04720db4beba71c68bf34bcd66d9a7b82d6
Dylan Wilson <dwilson5817@gmail.com>
2023-12-28 20:55:54 +0000

    feat(vault-ec2): create an EC2 instance to run HashiCorp vault on
    Create a new EC2 instance which will be used to install HashiCorp Vault
    on using Ansible.  Connection will be via EC2 Instance Connect so no SSH
    keys are added to this instance.

--- lib/infrastructure-vault-stack.ts
+++ lib/infrastructure-vault-stack.ts
@@ -1,16 +1,46 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class InfrastructureVaultStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const defaultVpc = ec2.Vpc.fromLookup(this, 'VPC', { isDefault: true })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureVaultQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const securityGroup = new ec2.SecurityGroup(
+        this,
+        'vault-instance-1-security-group',
+        {
+          vpc: defaultVpc,
+          allowAllOutbound: true,
+          securityGroupName: 'vault-instance-1-security-group',
+        }
+    )
+
+    securityGroup.addIngressRule(
+        ec2.Peer.anyIpv4(),
+        ec2.Port.tcp(22),
+        'Allows SSH access from Internet'
+    )
+
+    securityGroup.addIngressRule(
+        ec2.Peer.anyIpv4(),
+        ec2.Port.tcp(443),
+        'Allows HTTPS access from Internet'
+    )
+
+    new ec2.Instance(this, 'vault-instance-1', {
+      vpc: defaultVpc,
+      securityGroup: securityGroup,
+      instanceName: 'vault-instance-1',
+      instanceType: ec2.InstanceType.of(
+          ec2.InstanceClass.T2,
+          ec2.InstanceSize.MICRO
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+      }),
+    })
   }
 }
