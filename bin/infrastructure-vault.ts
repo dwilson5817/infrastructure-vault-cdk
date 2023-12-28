cf68df7c5d845d8332e2cf9919d121be71522be7
Dylan Wilson <dwilson5817@gmail.com>
2023-12-28 21:15:05 +0000

    ci(cdk-deploy): do not require approval on CDK deployment in CI
    The `cdk deploy` command will ask to confirm the changes to IAM policies
    as we are running this in a CI environment, we will use the
    `--require-approval=never` options to prevent this.

--- bin/infrastructure-vault.ts
+++ bin/infrastructure-vault.ts
@@ -15,7 +15,7 @@ new InfrastructureVaultStack(app, 'InfrastructureVaultStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '338348530174', region: 'eu-west-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
