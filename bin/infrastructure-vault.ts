e5446ce4af9a30dab474cc701c1367d901e31afc
Dylan Wilson <dwilson5817@gmail.com>
2025-06-01 15:49:09 +0000

    feat(cdk): update AWS account ID
    Moving this stack to a new AWS account.  Updated the AWS account ID
    accordingly.

--- bin/infrastructure-vault.ts
+++ bin/infrastructure-vault.ts
@@ -15,7 +15,7 @@ new InfrastructureVaultStack(app, 'InfrastructureVaultStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '183631325702', region: 'eu-west-1' },
+  env: { account: '197315783321', region: 'eu-west-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
